62ecde68199c358b3ad9f4fa2b66be855309f2fd
wldns0622 <wldns12378@gmail.com>
2022-02-15 22:55:24 +0900

    [Feat] middleware 추가

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 const app = express();
+
 // json parser
 app.use(express.json());
 // form data
 app.use(express.urlencoded());
+// cookie Read
+app.use(cookieParser());
+// log
+app.use(morgan('dev'));
+// security header
+app.use(helmet());
+// cors
 
 app.get('/', (req, res) => {
   return res.send('Welcome Hamkke!!!')
